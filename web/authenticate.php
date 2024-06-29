736ab4d2aaca563bffdd13f92c6822e204d4dac2
Xyberonzyx7 <advance.houdavid@outlook.com>
2024-06-29 15:13:50 -0700

    mod: make authenticate.php vulnerable to sqli

--- web/authenticate.php
+++ web/authenticate.php
@@ -7,46 +7,43 @@ $DATABASE_PASS = '';
 $DATABASE_NAME = 'phplogin';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if ( mysqli_connect_errno() ) {
-	// If there is an error with the connection, stop the script and display the error.
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+if (mysqli_connect_errno()) {
+    // If there is an error with the connection, stop the script and display the error.
+    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
 // Now we check if the data from the login form was submitted, isset() will check if the data exists.
-if ( !isset($_POST['username'], $_POST['password']) ) {
-	// Could not get the data that should have been sent.
-	exit('Please fill both the username and password fields!');
+if (!isset($_POST['username'], $_POST['password'])) {
+    // Could not get the data that should have been sent.
+    exit('Please fill both the username and password fields!');
 }
 
-// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
-	$stmt->bind_param('s', $_POST['username']);
-	$stmt->execute();
-	// Store the result so we can check if the account exists in the database.
-	$stmt->store_result();
-	if ($stmt->num_rows > 0) {
-		$stmt->bind_result($id, $password);
-		$stmt->fetch();
-		// Account exists, now we verify the password.
-		// Note: remember to use password_hash in your registration file to store the hashed passwords.
-		if ($_POST['password'] === $password) {
-			// Verification success! User has logged-in!
-			// Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
-			session_regenerate_id();
-			$_SESSION['loggedin'] = TRUE;
-			$_SESSION['name'] = $_POST['username'];
-			$_SESSION['id'] = $id;
-			header('Location: home.php');
-		} else {
-			// Incorrect password
-			echo 'Incorrect username and/or password!';
-		}
-	} else {
-		// Incorrect username
-		echo 'Incorrect username and/or password!';
-	}
+// Vulnerable to SQL injection (for demonstration only)
+$sql = "SELECT id, password FROM accounts WHERE username = '{$_POST['username']}' AND password = '{$_POST['password']}'";
+echo "SQL Query: " . $sql . PHP_EOL . "<br>";
 
-	$stmt->close();
+// Execute the query
+$result = mysqli_query($con, $sql);
+
+// Check if there is a result
+if ($result) {
+    // Store the result so we can check if the account exists in the database.
+    $row = mysqli_fetch_assoc($result);
+    if ($row) {
+            session_regenerate_id();
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['name'] = $_POST['username'];
+            $_SESSION['id'] = $row['id'];
+            header('Location: home.php');
+            exit;
+    } else {
+        echo 'Incorrect username and/or password!';
+    }
+} else {
+    // Error in executing the query
+    echo 'Error: ' . mysqli_error($con);
 }
+
+// Close the database connection
+mysqli_close($con);
 ?>
